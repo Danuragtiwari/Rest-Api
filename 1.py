a637699b88e4b2192e4f98dbc0a8a5caf708fde4
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-10-21 19:11:57 +0530

    API

---
+++ 1.py
@@ -0,0 +1,13 @@
+#API(application programming interface)
+# :- An api is a software intermediary that allows two or more application to talk to each
+
+# Api types in terms of Release Policies:-
+# 1.)Private:-It can be used within the organization.
+# 2.)Partner:-It can be used within Business Partners.
+# 3.)Public:-It can be used any third party Developers.
+
+# Examples:- 
+# Application(consumer)<---->Google Map API<---->Google Map(Provider)
+# Like you,waiter,chef.
+
+
