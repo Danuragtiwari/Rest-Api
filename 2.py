780ee7080dddd5b95a85ec6add803f1ac6ace6b4
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-10-21 19:27:18 +0530

    Web API

---
+++ 2.py
@@ -0,0 +1,15 @@
+# Web API
+# An API which is interface for web is called API
+# It may consist of one or more endpoints to define request and response
+# application(client)-->Web API -->Web application-->Database-->Web application-->Web API-->application
+# Client make http request to the API
+# API will communicate to web application/Database(if needed)
+# Database/Web Application provides required Data to API 
+# API returns Data(form is in:-JSON,XML) to Client.
+
+# How to use Web API
+# Register/Sign-up to API
+# API may provide API key for Authentication purpose
+# API key/Token :-*************
+# Whenever you need to communicte with server make request to API with API key.(url?key=**************)
+# If API key authentication succeed,API will provide required Data.
