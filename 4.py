dfe33cd80e12ffbd9c14534821a01ab27bd6a6d1
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-10-23 20:30:11 +0530

    Django Rest framework

---
+++ 4.py
@@ -0,0 +1,12 @@
+# Django REST Frameworks:-
+# Django rest framework is a powerful and flexible toolkit for building web APIs.
+# The Web browsable API is a huge usability win for your developers.
+# Authentication policies including packages for OAuth1 and OAuth2.
+# Serialization that supports both ORM and non-ORM data Sources.
+# Customizable all the way down-just use regular function-based views if you don't need the more powerful features.
+#Extensive doucmentation,and great community support.
+# Used and trusted by top notch companies:-Mozilla,Red-Hat
+
+# Requirements:-
+# python,django,django-restframework,django-filter.
+
