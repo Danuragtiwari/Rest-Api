2046199de9b99f3cabb4de65717dccd194304687
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-11-22 16:25:29 +0530

    CRUD API using Function Based View and Class Based View in Django REST Framework

--- gs4/gs4/urls.py
+++ gs4/gs4/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('studentapi/',views.student_api),
+    path('studentapi/',views.StudentAPI.as_view()),
 ]
+
