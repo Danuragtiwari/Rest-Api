ade2a6accd7a6eda0dcbb2fea074d8a053873aaa
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-11-27 02:18:17 +0530

    Class Based APIView in Django REST Framework

--- gs12/gs12/urls.py
+++ gs12/gs12/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('studentapi/',views.student_api),
-    path('studentapi/<int:pk>',views.student_api),
+    path('studentapi/',views.StudentAPI.as_view()),
+    path('studentapi/<int:pk>',views.StudentAPI.as_view()),
 ]
