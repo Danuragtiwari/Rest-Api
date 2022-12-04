7db36a554478e1f5702b589d2f4e9c74cfaa65a5
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-12-04 10:17:49 +0530

    Session Authentication and Permission in Django REST Framework

---
+++ gs21/api/views.py
@@ -0,0 +1,32 @@
+# ModelViewSet 
+from .models import Student
+from .serializers import StudentSerializer 
+from rest_framework import viewsets  
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
+
+
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+    authentication_classes=[SessionAuthentication]
+    # permission_classes=[IsAuthenticated]
+     # permission_classes=[AallowAny]
+    # permission_classes=[IsAdminUser]
+    # permission_classes=[IsAuthenticatedOrReadOnly]
+    # permission_classes=[DjangoModelPermissions]
+    permission_classes=[DjangoModelPermissionsOrAnonReadOnly]
+   
+    
+# class StudentModelViewSet1(viewsets.ModelViewSet):
+#     queryset=Student.objects.all()
+#     serializer_class=StudentSerializer
+   
+
+# class StudentModelViewSet2(viewsets.ModelViewSet):
+#     queryset=Student.objects.all()
+#     serializer_class=StudentSerializer
+#     authentication_classes=[BasicAuthentication]
+#     permission_classes=[AllowAny]
+   
