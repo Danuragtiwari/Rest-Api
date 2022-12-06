44eacbd76f20fb5f117162aefb65a9f0706547fd
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-12-07 00:57:49 +0530

    Token Authentication in Django REST Framework

--- gs28/api/views.py
+++ gs28/api/views.py
@@ -3,7 +3,7 @@ from .models import Student
 from .serializers import StudentSerializer 
 from rest_framework import viewsets  
 from rest_framework.authentication import TokenAuthentication,SessionAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
 
 
 
@@ -11,5 +11,6 @@ class StudentModelViewSet(viewsets.ModelViewSet):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
     authentication_classes=[TokenAuthentication]
-    permission_classes=[IsAuthenticated]
+    # permission_classes=[IsAuthenticated]
+    permission_classes=[IsAuthenticatedOrReadOnly]
    
