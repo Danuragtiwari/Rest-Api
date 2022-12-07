f8c1f9d1e7c51baf6873e285b011120507e443fd
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-12-07 21:09:23 +0530

    JSON Web Token and Simple JWT in Django REST Framework

---
+++ 22.py
@@ -0,0 +1,15 @@
+# Authentication:-
+# Third Party Packages:
+# JSON web Token Authentication:-
+#JSON web token is a fairly new standard which can be used for token-based authentication.unlike the built-in TokenAuthentication scheme,JWT Authentication doesn't need to use a database to validate a token.
+# please refers:- google for more info
+
+#Simple JWT:-
+# Simple JWT provides a JSON web Token Authentication backend for the Django Rest Framework.It aims to cover the most common use cases of JWT's by offering a conservative set of default features.It also aims to be easily extensible in case a desired features is not present.
+# https://django-rest-framework-simplyjwt.readthedocs.io/en/latest/
+
+# How to install Simple JWT
+# pip install djangorestframework-simplejwt
+
+# How to configure Simple JWT:-
+  
