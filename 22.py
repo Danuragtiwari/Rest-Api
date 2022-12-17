af28330df35cab03d3eaca1383334068ca50f89a
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-12-17 22:10:49 +0530

    JSON Web Token and Simple JWT in Django REST Framework

--- 22.py
+++ 22.py
@@ -12,4 +12,57 @@
 # pip install djangorestframework-simplejwt
 
 # How to configure Simple JWT:-
-  
+# settings.py :- it is globlly define for every views
+#  REST_FRAMEWORK={'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework_simplejwt.authentication.JWTAuthentication',)}
+
+# urls.py
+# from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView 
+# urlpatterns=[
+# path('gettoken',TokenObtainPairView.as_view(),name='token_obtain_pair'),
+# path('refreshtoken/',TokenRefreshView.as_view(),name='token_refresh'),
+# ]
+
+
+# To verify:-
+# You can also include a route for simple JWT's Token VerifyView if you wish to allow API users to verify HMAC-signed tokens without having access to your signing key.
+# urls.py
+# from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
+# urlpatterns=[
+# path('gettoken/',TokenObtainView.as_view(),name='token_obtain_pair'),
+# path('refreshtoken/',TokenRefreshView.as_view(),name='token_refresh'),
+# path('verifytoken/',TokenVerifyView.as_view(),name='token_verify'),
+# ]
+
+# JWT Default Settings:-
+# from datetime import timedelta 
+# SIMPLE_JWT={'ACCESS_TOKEN_LIFETIME':timedelta(minutes=5),'REFRESH_TOKEN_LIFETIME':timedelta(days=1),'ROTATE_REFRESH_TOKENS':False,'BLASKLIST_AFTER_ROTATION;:True,
+# 'ALGORITHM':'HS256','SIGNING_KEY':settings.SECRET_KEY,'VERIFYING_KEY':None,'AUDIENCE':None,'AUTH_KEADER_TYPES':('Bearer',),'USER_ID_FIELD':'id','USER_ID_CLAIM':'user_id','AUTH_TOKEN_CLASSES':('rest_framework_simplejwt.tokens.AccessToken',),'TOKEN_TYPE_CLAIM':'token_type','JTI_CLAIM':'jti','SLIDING_TOKEN_REFRESH_EXP_CLAIM':'refresh_exp','SLIDING_TOKEN_LIFETIME':timedelta(minutes=5),}
+
+# ACCESS_TOKEN_LIFETIME:- a datetime.timedelta object which specifies  how long access tokens are valid.
+
+# REFRESH_TOKEN_LIFETIME:- A datetime.timedelta object which specifies how long refresh tokens are valid.
+ 
+# Use JWT  
+# Get Token 
+# from urls write,urlpatterns=[path('gettoken/',)]
+# http POST http://127.0.0.1:8000/gettoken/ username='user1 password='Danurag23@'
+# for verify Token
+# http POST http://127.0.0.1:8000/verifytoken/ token='uddhcvuygruygkydvghdggvkdgvmdhgkuy336287trwf ghegfjygeyfgehj'
+# we get OK for verify else bad request .
+# Refresh Token
+# http POST http://127.0.0.1:8000/refreshtoken/ refresh='qpokfwbkhdghjbscvgv661218y928y98qhwui78'
+
+
+# Permission Classes:- Permission in REST framework are always defined as alist of permission classes. 
+# AllowAny 
+# IsAuthenticated
+# IsAdminUser
+# IsAuthenticatedOrReadOnly
+# DjangoModelPermissions
+#DjangoModelPermissionsOrAnonReadOnly
+# DjangoObjectPermissions 
+# Custom Permissions
+
+
+# to get 
+#  http http://127.0.0.1:8000/studentapi/ 'Authorization:Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcxMjk0NTY5LCJpYXQiOjE2NzEyOTMxMDIsImp0aSI6ImQxYmNlMTQ3YTEyZTRhMTA5ZjA0NWY1Njc5ZDc0NjQxIiwidXNlcl9pZCI6MX0.OKyXrxPKnO66u38-WSVZ88zKUmdqIN29x4PNiTfZIDs'
